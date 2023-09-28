bf57a9a83ca4f5d35f0583e42bf9b20b860d3183
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2023-09-28 21:35:03 +0530

    custom fonts

--- src/HomeScreen/TravelCustomText.tsx
+++ src/HomeScreen/TravelCustomText.tsx
@@ -1,21 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
 
-const TravelCustomText = (props:any) => {
+const TravelCustomText = (props: any) => {
   return (
     <View>
-      <Text style={{
-        fontFamily:'quicksands',
-        fontStyle:'italic',
-        fontSize:50,
-        color:'#73CCCC',
-        fontWeight:'700',
-        position:'absolute',
-        marginLeft:10,
-        marginTop:50
-      }}>{props.children}</Text>
+      <Text style={styles.customText}>{props.children}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default TravelCustomText
+const styles = StyleSheet.create({
+  customText: {
+    fontFamily: 'Sriracha-Regular', 
+    fontStyle: 'italic',
+    fontSize: 50,
+    color: '#73CCCC',
+    fontWeight: '700',
+    position: 'absolute',
+    marginLeft: 10,
+    marginTop: 50,
+  },
+});
+
+export default TravelCustomText;
