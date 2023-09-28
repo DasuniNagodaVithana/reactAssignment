e08b22d430ef6d1902c8fb9b2d78c2f4e18fbf11
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2023-09-28 18:59:12 +0530

    get started button

--- src/HomeScreen/WhiteRectangle.tsx
+++ src/HomeScreen/WhiteRectangle.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 
 function WhiteRectangle() {
+  function alert(arg0: string): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <View style={styles.rectangle}>
-      <Text style={styles.whiteText}>let's explore</Text>
+      <Text style={styles.whiteText}>Let's explore</Text>
+      <Text style={styles.largeWhiteText}>Your Dream Vacation</Text>
+
+      <TouchableOpacity style={styles.button} onPress={() => alert('Get Started clicked!')}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,14 +22,33 @@ const styles = StyleSheet.create({
   rectangle: {
     height: 230,
     backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    padding: 20, 
-    alignItems: 'center', 
-    justifyContent: 'center', 
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   whiteText: {
     color: '#000',
     fontSize: 18,
     fontWeight: 'bold',
+  },
+  largeWhiteText: {
+    color: '#000',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20, 
+  },
+  button: {
+    backgroundColor: '#000',
+    padding: 15,
+    borderRadius: 54,
+    width:200,
+    
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
 
