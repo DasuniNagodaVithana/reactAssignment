7a47f2d8e7f5e69e92f91f1070cd01d4849c05ee
DasuniNagodaVithana <130269542+DasuniNagodaVithana@users.noreply.github.com>
2023-09-29 15:26:47 +0530

    adding all files together to make navigation

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import LockScreen from './NewHomeScreen/LockScreen';
+import Homepage from './NewHomeScreen/Homepage';
+import ViewPage from './NewHomeScreen/Viewpage';
+import Logout from './NewHomeScreen/Logout';
+
+const App = () => {
+  // Your app code here
+
+  return (
+    
+    <Logout /> 
+    
+  );
+};
+
+export default App;
+
